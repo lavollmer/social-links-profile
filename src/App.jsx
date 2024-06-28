0200ef3773d2e59230b1feb2da33d9fdc456705c
Laura <laura.e.vollmer@gmail.com>
2024-06-28 14:47:13 -0500

    adding profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import profilePic from "./assets/avatar-jessica.jpeg";
 
 function App() {
   return (
-    <div className="bg-black">
-      <h1 className="font-inter text-neutral-white">Jessica Randall</h1>
-      <h2 className="font-inter text-primary-green">London, United Kingdom</h2>
+    <div>
+      <img src={profilePic} alt="Profile Picture of Jessica" className="rounded"/>
+
+      <div className="bg-black">
+        <h1 className="font-inter text-2xl text-neutral-white">
+          Jessica Randall
+        </h1>
+        <h2 className="font-inter text-primary-green">
+          London, United Kingdom
+        </h2>
+      </div>
     </div>
   );
 }
