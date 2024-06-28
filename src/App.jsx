884fcf37495532d390043730ea2f2ecdc1e90ad0
Laura <laura.e.vollmer@gmail.com>
2024-06-28 14:43:45 -0500

    added the client's name and location in style

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="font-inter text-primary-green">
-      This is a text in Inter font.
+    <div className="bg-black">
+      <h1 className="font-inter text-neutral-white">Jessica Randall</h1>
+      <h2 className="font-inter text-primary-green">London, United Kingdom</h2>
     </div>
   );
 }
